a6a6acb08c47cc450f6d8cac95d4fb5988f8452c
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-01-06 04:26:30 +0200

    add binary and hex number formatting

--- src/number-formatter.ts
+++ src/number-formatter.ts
@@ -19,19 +19,88 @@ function getFormatterFromSpecifiers(
 	locale: string,
 	specifier: string
 ): NumberFormatter {
-	const key = `${locale}/${specifier}`;
+	const s = specifier[0];
+	const digits =
+		specifier.length > 1 ? parseInt(specifier.slice(1), 10) : undefined;
+	if (digits !== undefined && (Number.isNaN(digits) || digits < 0)) {
+		throw new Error(`Invalid number format specifier: ${specifier}`);
+	}
+	const localeIndependent = 'bBxX'.indexOf(s) >= 0;
+	const key = localeIndependent ? specifier : `${locale}/${specifier}`;
+
 	let formatter = formatterCache.get(key);
 	if (!formatter) {
-		const options = getOptionsFromSpecifier(specifier);
-		formatter = Intl.NumberFormat(locale, options).format;
+		if (localeIndependent) {
+			formatter = getLocaleIndependentFormatter(s, digits);
+		} else {
+			const options = getOptionsFromSpecifier(s, digits);
+			formatter = Intl.NumberFormat(locale, options).format;
+		}
 		formatterCache.set(key, formatter);
 	}
 	return formatter;
 }
 
-function getOptionsFromSpecifier(specifier: string): Intl.NumberFormatOptions {
+function throwIfNotInteger(value: number): void {
+	if (!Number.isInteger(value)) {
+		throw new Error('Number must be an integer');
+	}
+}
+
+function getIntegerFormatter(
+	radix: number,
+	digits: number | undefined
+): NumberFormatter {
+	return value => {
+		throwIfNotInteger(value);
+		if (value < -2147483648) {
+			throw new Error('Number is too small to be represented');
+		}
+		if (value < 0) {
+			let result = '';
+			for (let i = 0; i < 8; i++) {
+				let byte = (value & 0xf).toString(radix);
+				if (radix === 2) {
+					byte = byte.padStart(4, '0');
+				}
+				result = byte + result;
+				value = value >> 4;
+			}
+			return result;
+		}
+
+		return digits
+			? value.toString(radix).padStart(digits, '0')
+			: value.toString(radix);
+	};
+}
+
+function getLocaleIndependentFormatter(
+	specifier: string,
+	digits: number | undefined
+): NumberFormatter {
+	if (specifier === 'b' || specifier === 'B') {
+		return getIntegerFormatter(2, digits);
+	} else if (specifier === 'x') {
+		return getIntegerFormatter(16, digits);
+	} else if (specifier === 'X') {
+		const fmt = getIntegerFormatter(16, digits);
+		return value => fmt(value).toUpperCase();
+	}
+	throw new Error(
+		`Invalid locale-independent number format specifier: ${specifier}`
+	);
+}
+
+function getOptionsFromSpecifier(
+	specifier: string,
+	digits: number | undefined
+): Intl.NumberFormatOptions {
 	if (specifier === '' || specifier === 'd') {
-		return {};
+		return {
+			useGrouping: true,
+			maximumFractionDigits: digits,
+		};
 	}
 
 	// TODO: Implement this function
--- test/number-formatter.test.ts
+++ test/number-formatter.test.ts
@@ -8,4 +8,62 @@ describe('Number formatting', () => {
 		const formatter = numberFormatter('en-US');
 		assert.strictEqual(formatter(1234567.89), '1,234,567.89');
 	});
+
+	it('Throws an error formatting a non integer with "b" specifier', () => {
+		const formatter = numberFormatter('en-US', 'b');
+		assert.throws(() => formatter(1.2), {
+			message: 'Number must be an integer',
+		});
+	});
+
+	it('Correctly formats using "b" specifier', () => {
+		const formatter = numberFormatter('en-US', 'b');
+		assert.strictEqual(formatter(123), '1111011');
+	});
+
+	it('Correctly formats using "b5" specifier', () => {
+		const formatter = numberFormatter('en-US', 'b5');
+		assert.strictEqual(formatter(4), '00100');
+	});
+
+	it('Correctly formats negative number using "b" specifier', () => {
+		const formatter = numberFormatter('en-US', 'b5');
+		assert.strictEqual(formatter(-4), '11111111111111111111111111111100');
+	});
+
+	it('Throws on formatting min negative number using "b" specifier', () => {
+		const formatter = numberFormatter('en-US', 'b');
+		assert.throws(() => formatter(Number.MIN_SAFE_INTEGER), {
+			message: 'Number is too small to be represented',
+		});
+	});
+
+	it('Throws an error formatting a non integer with "x" specifier', () => {
+		const formatter = numberFormatter('en-US', 'x');
+		assert.throws(() => formatter(1.2), {
+			message: 'Number must be an integer',
+		});
+	});
+
+	it('Correctly formats using "x" specifier', () => {
+		const formatter = numberFormatter('en-US', 'x');
+		assert.strictEqual(formatter(123), '7b');
+	});
+
+	it('Correctly formats using "x5" specifier', () => {
+		const formatter = numberFormatter('en-US', 'x5');
+		assert.strictEqual(formatter(4), '00004');
+	});
+
+	it('Correctly formats negative number using "x" specifier', () => {
+		const formatter = numberFormatter('en-US', 'x5');
+		assert.strictEqual(formatter(-2140999224), '8062f1c8');
+	});
+
+	it('Throws on formatting min negative number using "x" specifier', () => {
+		const formatter = numberFormatter('en-US', 'b');
+		assert.throws(() => formatter(Number.MIN_SAFE_INTEGER), {
+			message: 'Number is too small to be represented',
+		});
+	});
 });
