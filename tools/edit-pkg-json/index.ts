cf84617f607d0f7928bbf8705b0c7a364e50ac1c
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-03-08 04:44:12 +0200

    update build to automate versioning.

--- tools/edit-pkg-json/index.ts
+++ tools/edit-pkg-json/index.ts
@@ -1,6 +1,7 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { exec } from 'node:child_process';
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename);
@@ -11,8 +12,32 @@ const buildPackageJsonPath = path.resolve(
 	'../../build/package.json'
 );
 
+const versionRx = /^v(\d+\.\d+\.\d+)(?:-(\d+)-(g[a-f0-9]+))?$/;
+const version = await new Promise<string>((resolve, reject) => {
+	exec('git describe --tags', {}, (error, stdout) => {
+		if (error) {
+			reject(error);
+		} else {
+			resolve(stdout.trim());
+		}
+	});
+});
+
+const match = version.match(versionRx);
+if (!match) {
+	console.error(`Invalid version: ${version}`);
+	process.exit(1);
+}
+
+let actualVersion = match[1];
+if (match[2]) {
+	actualVersion += `-build.${match[2]}`;
+}
+
+console.log(`Version: ${version}`);
+
 // Read the package.json file
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf-8'));
 
 // Remove scripts and devDependencies
 packageJson.scripts = {};
@@ -20,8 +45,11 @@ packageJson.devDependencies = {};
 packageJson.private = false;
 packageJson.engines = {};
 
+// Update the version
+packageJson.version = actualVersion;
+
 // Write the modified package.json to the build directory
-fs.writeFileSync(
+await fs.writeFile(
 	buildPackageJsonPath,
 	JSON.stringify(packageJson, null, 2),
 	'utf-8'
