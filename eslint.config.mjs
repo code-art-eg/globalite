7f3c9c03aa64b09a3e4559e54a71415769a2e7a7
sherif-elmetainy <sherif.elmetainy@gmail.com>
2026-09-08 05:29:02 +0300

    update packages and fix build errors caused by wrong lib in tsconfig.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,9 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+	{
+		ignores: ['docs/**', 'build/**'],
+	},
 	eslint.configs.recommended,
 	tseslint.configs.recommended,
 );
